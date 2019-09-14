23b765e0d0ebd9208e22245d32d7aa77fd257d0f
Chris Andersson <candersson@klarsolutions.com>
2019-09-14 15:56:50 -0400

    finished sign in and sign up

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 
 class SignIn extends React.Component {
@@ -15,14 +15,26 @@ class SignIn extends React.Component {
 		}
 	}
 
-	handleSubmit = event => {
+	handleSubmit = async event => {
 		event.preventDefault();
-		this.setState({email: '', password: ''})
+		
+		const { email, password } = this.state;
+
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({email: '', password: ''});
+		} catch (error) {
+			console.log(error);
+		}
+
+		
 	}
 
 	handleChange = event => {
 		const { value, name } = event.target;
+	
 		this.setState({ [name]: value })
+
 
 	}
 
@@ -32,7 +44,7 @@ class SignIn extends React.Component {
 				<h2> I already have an account</h2>
 				<span> Sign in with your email and password </span>
 
-				<form onSubmit={this.handeSubmit}>
+				<form onSubmit={this.handleSubmit}>
 					<FormInput 
 						name="email" 
 						type="email"
